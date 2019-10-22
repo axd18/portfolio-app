d6da8200336698427a8a17c237e511c30e227f9c
adrijfer18 <adrianfernandezj@gmail.com>
2019-10-21 23:38:01 -0300

    feature: Creando las rutas, editando el menú y uso de hash en las rutas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+
+const app_routes: Routes = [
+    {path: 'home',  component: PortafolioComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'item', component: ItemComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'},
+
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot ( app_routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+  })
+ export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { ItemComponent } from './pages/item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
