3dfa893c7c99f24bd4f54dac66272c7d5c456523
adrijfer18 <adrianfernandezj@gmail.com>
2019-10-26 14:38:22 -0300

    feat: utilizando Firebase para nuestra automatizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { InfoPaginaService } from './services/info-pagina.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor( public InfoPaginaService: InfoPaginaService){
-    
+  constructor( public InfoPaginaService: InfoPaginaService) {
+
   }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -2,21 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
     // console.log('Servicio de infoPagina listo');
+    // this.cargarInfo();
+    this.cargarEquipo();
 
-    // Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp: InfoPagina) => {
-          this.cargada = true;
-          this.info = resp;
-          console.log(resp);
-        });
   }
-}
+
+    /*private cargarInfo() {
+      // Leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.equipo = true;
+        this.info = resp;
+      });
+    }*/
+
+    private cargarEquipo() {
+      this.http.get('https://angular-html-f5321.firebaseio.com/equipo.json')
+      .subscribe((resp: any []) => {
+        this.equipo = resp;
+        console.log(resp);
+      });
+    }
+  }
